7c6bd9b06f22279358dc3bba1b13b9cebb6f46fd
guofazhan <guofazhan@hotmail.com>
2019-02-22 16:37:51 +0800

    ui工程开发,添加基础功能

--- ump-app/ump-app-ui/src/views/header-aside-layout/components/aside-bottom/AsideBottom.vue
+++ ump-app/ump-app-ui/src/views/header-aside-layout/components/aside-bottom/AsideBottom.vue
@@ -1,10 +1,33 @@
 <template>
-    <div class="aside-bottom">菜单导航</div>
+    <ump-footer height="40px" style="padding: 5px" class="aside-bottom"
+                flex="dir:right">
+        <section flex-box="0" class="aside-bottom-btn"
+                 @click="handleToggleAside">
+            <font-awesome-icon v-if="asideCollapse"
+                               :icon="['fas','angle-right']"
+                               size="xs"></font-awesome-icon>
+            <font-awesome-icon v-else :icon="['fas','angle-left']"
+                               size="xs"></font-awesome-icon>
+        </section>
+    </ump-footer>
 </template>
 
 <script>
+    import {mapState, mapActions} from 'vuex'
+
     export default {
-        name: "AsideBottom"
+        name: "AsideBottom",
+        methods: {
+            ...mapActions('portal/common', {
+                'handleToggleAside': 'toggleAsideCollapse'
+            })
+
+        },
+        computed: {
+            ...mapState('portal/common', {
+                asideCollapse: state => state.asideCollapse
+            }),
+        }
     }
 </script>
 
--- ump-app/ump-app-ui/src/views/header-aside-layout/components/aside-menu/AsideMenu.vue
+++ ump-app/ump-app-ui/src/views/header-aside-layout/components/aside-menu/AsideMenu.vue
@@ -1,13 +1,156 @@
 <template>
-    <div class="aside-menu">菜单部分</div>
+    <div class="aside-menu">
+        <SideMenu :style="{width:width}"
+                  :data="menus"
+                  :collapsed="asideCollapse">
+        </SideMenu>
+    </div>
 </template>
 
 <script>
+    import {mapState, mapActions} from 'vuex'
+
     export default {
-        name: "AsideMenu"
+        name: "AsideMenu",
+        computed: {
+            ...mapState('portal/common', {
+                asideCollapse: state => state.asideCollapse
+            }),
+            width: function () {
+                return this.asideCollapse ? "65px" : "200px";
+            }
+        },
+        data() {
+            return {
+                selection: null,
+                menus: [
+                    {
+                        text: "Forms",
+                        iconCls: "fa fa-wpforms",
+                        state: "open",
+                        children: [
+                            {
+                                text: "Form Element"
+                            },
+                            {
+                                text: "Wizard"
+                            },
+                            {
+                                text: "File Upload"
+                            }
+                        ]
+                    },
+                    {
+                        text: "Mail",
+                        iconCls: "fa fa-at",
+                        selected: true,
+                        children: [
+                            {
+                                text: "Inbox"
+                            },
+                            {
+                                text: "Sent"
+                            },
+                            {
+                                text: "Trash",
+                                children: [
+                                    {
+                                        text: "Item1"
+                                    },
+                                    {
+                                        text: "Item2"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        text: "Layout",
+                        iconCls: "fa fa-table",
+                        children: [
+                            {
+                                text: "Panel"
+                            },
+                            {
+                                text: "Accordion"
+                            },
+                            {
+                                text: "Tabs"
+                            }
+                        ]
+                    }
+                ]
+            };
+        },
     }
 </script>
 
-<style scoped>
+<style type="text/css">
+    .sidemenu .accordion .panel-title {
+        color: #b8c7ce;
+    }
 
+    .sidemenu .accordion .accordion-header {
+        background: #222d32;
+        color: #b8c7ce;
+    }
+
+    .sidemenu .accordion .accordion-body {
+        background: #2c3b41;
+        color: #8aa4af;
+    }
+
+    .sidemenu .accordion .accordion-header-selected {
+        background: #1e282c;
+    }
+
+    .sidemenu .tree-node-hover {
+        background: #2c3b41;
+        color: #fff;
+    }
+
+    .sidemenu .tree-node-selected {
+        background: #2c3b41;
+        color: #fff;
+    }
+
+    .sidemenu .accordion-header .panel-tool {
+        display: none;
+    }
+
+    .sidemenu .accordion-header::after,
+    .sidemenu .tree-node-nonleaf::after {
+        display: inline-block;
+        vertical-align: top;
+        border-style: solid;
+        transform: rotate(45deg);
+        width: 4px;
+        height: 4px;
+        content: "";
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        margin-top: -3px;
+        border-width: 0 1px 1px 0;
+    }
+
+    .sidemenu .accordion-header-selected::after {
+        transform: rotate(-135deg);
+    }
+
+    .sidemenu .tree-node-nonleaf::after {
+        transform: rotate(-135deg);
+    }
+
+    .sidemenu .tree-node-nonleaf-collapsed::after {
+        transform: rotate(45deg);
+    }
+
+    .sidemenu-collapsed .accordion-header::after {
+        display: none;
+    }
+
+    .sidemenu-tooltip .accordion {
+        border-color: #1e282c;
+    }
 </style>
